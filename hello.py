a5c66e15459b66da7f1dd756403b7a076aa77520
julio-barbieri <barbieri.code@gmail.com>
2020-12-13 12:43:55 -0300

    A segunda é mais relevante

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 def Pessoa(nome):
     return f"Bem-vindo {nome}"
 
+def Carro(marca, modelo, ano):
+    return f" dirige um {marca} {modelo} ano {ano} cor chumbo"
+
 
 print(Pessoa("Julio"))
+print(Carro("Fiat", "Argo", "2021"))
