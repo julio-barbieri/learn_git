efc3490058b8e2e239f8acc8c0f6c9fb1e68d6de
julio-barbieri <barbieri.code@gmail.com>
2020-12-13 14:37:19 -0300

    Teste de implementação da função de calculo de média de combústivel

--- hello.py
+++ hello.py
@@ -2,8 +2,12 @@ def Pessoa(nome):
     return f"Bem-vindo {nome}"
 
 def Carro(marca, modelo, ano):
-    return f" dirige um {marca} {modelo} ano {ano} cor chumbo"
+    return f"Você dirige um {marca} {modelo} ano {ano} cor chumbo"
 
+
+def CalculaMedia(km, litros):
+    return f"Seu veículo faz em média {km / litros} quilometros por litro";
 
 print(Pessoa("Julio"))
 print(Carro("Fiat", "Argo", "2021"))
+print(CalculaMedia(500, 45))
